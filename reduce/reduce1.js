467380f05a9ff0d69200c9494b6394e0f793e688
Vijayamrav <95993395+Vijayamrav@users.noreply.github.com>
2024-06-23 22:12:53 +0530

    changed code

--- reduce/reduce1.js
+++ reduce/reduce1.js
@@ -1,27 +1,27 @@
-// arr=[1,2,3,4,5,6,7];
-// const sum=arr.reduce((a,b)=>a+b,1);
-// console.log(sum);
+arr=[1,2,3,4,5,6,7];
+const sum=arr.reduce((a,b)=>a+b,1);
+console.log(sum);
 
-// array=[1,2,3,4,5];
-// array.reduce((a,b)=>b*b);
-// console.log(array);
-// debugger;
-// arr=[1,2,3,4,5,6];
-// var sum=arr.reduce((acc,currentValue,currentIndex,array)=>{
-//        acc=acc+currentValue;
-//        if(currentIndex==array.length-1){
-//          return acc/array.length;
-//        }
-//        return acc;
-// },0)
-// console.log(sum);
+array=[1,2,3,4,5];
+array.reduce((a,b)=>b*b);
+console.log(array);
+debugger;
+arr=[1,2,3,4,5,6];
+var sum=arr.reduce((acc,currentValue,currentIndex,array)=>{
+       acc=acc+currentValue;
+       if(currentIndex==array.length-1){
+         return acc/array.length;
+       }
+       return acc;
+},0)
+console.log(sum);
 
-// arr=[1,1,2,2,3,3];
-// arr.reduce((acc,currentValue)=>{
-//     if(acc==currentValue){
+arr=[1,1,2,2,3,3];
+arr.reduce((acc,currentValue)=>{
+    if(acc==currentValue){
 
-//     }
-// })
+    }
+})
 const letters = [1,1,2,2,3,3];
 const letterCounts = letters.reduce((accumulator, currentValue) => {
   accumulator[currentValue] = (accumulator[currentValue] || 0) + 1;
